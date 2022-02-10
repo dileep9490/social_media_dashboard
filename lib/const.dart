8729581c583982f65ab036202e7d5d10a572426a
dileep kumar <63330310+dileep9490@users.noreply.github.com>
2022-02-10 10:40:19 +0530

    added colors

---
+++ lib/const.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+
+//!Primary Colors
+const Color limeGreen = Color(0xff1db489);
+const Color brightRed = Color(0xffdc414c);
+const Color facebook = Color(0xff198ff5);
+const Color twitter = Color(0xff1ca0f2);
+const Color instagram1 = Color(0xfffdc468);
+const Color instagram2 = Color(0xffdf4996);
+const Color youtube = Color(0xffc4032a);
+const Color limegreen = Color(0xff1db489);
+
+//!Toggle
+//* for dark theme it's gradient
+const Color toggledark1 = Color(0xff378fe6);
+const Color toggledark2 = Color(0xff3eda82);
+//* for light theme
+const Color toggleLight = Color(0xffaeb3cb);
+
+
+//! Dark Theme
+const Color darkBG = Color(0xff1e202a);
+const Color topDarkBG = Color(0xff1f212e);
+const Color darkCardBG = Color(0xff252a41);
+const Color darkText = Color(0xff8b97c6);
+const Color darkText2 = Color(0xffffffff);
+
+//! Light Theme
+const Color whiteBG = Color(0xffffffff);
+const Color topLightBG = Color(0xfff5f7ff);
+const Color lightCardBg = Color(0xfff0f2fa);
+const Color lightText1 = Color(0xff63687e);
+const Color lightText2 = Color(0xff1e202a);
